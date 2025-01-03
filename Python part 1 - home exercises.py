bb1530bfe5b4f8f1643a768ff8be5d40c99d3f4a
Sudarshen <sudarshenk@gmail.com>
2025-01-03 14:33:56 +0100

    Doing homework Python exercises part 1.

---
+++ Python part 1 - home exercises.py
@@ -0,0 +1,17 @@
+# Exercise 1:
+
+# print("""Twinkle, twinkle, little star, 
+#         How I wonder what you are! 
+#                 Up above the world so high,
+#                 Like a diamond in the sky.
+# Twinkle, twinkle, little star,
+#         How i wonder what you are.""")
+
+# print("Twinkle, twinkle, little star,\n"
+#       "        How I wonder what you are!\n"
+#       "                Up above the world so high,\n"
+#       "                Like the diamond in the sky.\n"
+#       "Twinkle, twinkle, little star,\n"
+#       "        How I wonder what you are.")\
+
+# Exercise 2:
